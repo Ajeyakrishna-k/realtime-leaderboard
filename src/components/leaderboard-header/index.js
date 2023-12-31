8aa059692584e61f823667ac48bb6640c15de6ae
Ajeyakrishna <connect.ajeya@gmail.com>
2023-12-31 08:08:55 +0530

    feat: adds leaderboard header

---
+++ src/components/leaderboard-header/index.js
@@ -0,0 +1,10 @@
+import styles from "./leaderboard-header.module.css";
+
+const LeaderboardHeader = () => {
+  return (
+    <header className={styles.header}>
+      <h1 className={styles.title}>Leaderboard</h1>
+    </header>
+  );
+};
+export default LeaderboardHeader;
